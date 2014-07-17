b6a738708f366ef47eaa353172a8af8fd288e281
Sebastian Sito <sebastian@hypenode.com>
2014-07-17 19:03:14 +0200

    payu notify reply [#74609506]

--- app/controllers/spree/payu_controller.rb
+++ app/controllers/spree/payu_controller.rb
@@ -1,6 +1,18 @@
 module Spree
 	class PayuController < Spree::BaseController
 		def notify
+			@response = OpenPayU::Order.consume_notification(request)
+
+			case @response.order_status
+			when 'NEW'
+			when 'PENDING'
+			when 'CANCELLED'
+			when 'REJECTED'
+			when 'COMPLETED'
+			when 'WAITING_FOR_CONFIRMATION'
+			end
+
+			render json: OpenPayU::Order.build_notify_response(@response.req_id)
 		end
 
 		def continue
