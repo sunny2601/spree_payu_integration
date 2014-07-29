f764cf688b189705b0787164057e537d64b524d7
Sebastian Sito <sebastian@hypenode.com>
2014-07-29 12:20:35 +0200

    payu controller refactor

--- app/controllers/spree/payu_controller.rb
+++ app/controllers/spree/payu_controller.rb
@@ -3,31 +3,24 @@ module Spree
     protect_from_forgery except: [:notify, :continue]
 
     def notify
-      @response = OpenPayU::Order.retrieve(params[:order][:orderId])
-      @order_info = response.parsed_data['orders']['orders'].first
-      @payment = Spree::Order.find(order_info['extOrderId']).payments.last
+      response = OpenPayU::Order.retrieve(params[:order][:orderId])
+      order_info = response.parsed_data['orders']['orders'].first
+      order = Spree::Order.find(order_info['extOrderId'])
+      payment = order.payments.last
 
-      if @payment.completed?
-        render json: OpenPayU::Order.build_notify_response(response.req_id)
-      else
-        complete_payment
+      unless payment.completed?
+        case order_info['status']
+        when 'CANCELED', 'REJECTED'
+          payment.failure!
+        when 'COMPLETED'
+          payment.complete!
+        end
       end
+
+      render json: OpenPayU::Order.build_notify_response(response.req_id)
     end
 
     def continue
-    end
-
-  private
-
-    def complete_payment
-      case @order_info['status']
-      when 'CANCELED', 'REJECTED'
-        @payment.failure!
-      when 'COMPLETED'
-        @payment.complete!
-      end
-
-      render json: OpenPayU::Order.build_notify_response(@response.req_id)
     end
   end
 end
